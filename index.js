974eb7adb34bbbe441bbbb66c0e64bb00e9e2b4e
tuhin180 <tanjirtuhin18@gmail.com>
2022-10-21 08:02:07 +0600

    server update

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 const categories = require("./data/categories.json");
 
+app.use(cors());
+const news = require("./data/news.json");
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/news-categories", (req, res) => {
   res.send(categories);
+});
+app.get("/news", (req, res) => {
+  res.send(news);
 });
 
 app.listen(port, () => {
