72ac21753bb0a26fca5ffd0e1dec66e477fb64ad
tuhin180 <tanjirtuhin18@gmail.com>
2022-10-23 00:02:42 +0600

    done

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 const categories = require("./data/categories.json");
-
-app.use(cors());
 const news = require("./data/news.json");
 
 app.get("/", (req, res) => {
@@ -16,8 +14,22 @@ app.get("/", (req, res) => {
 app.get("/news-categories", (req, res) => {
   res.send(categories);
 });
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const categoriesNews = news.filter((item) => item.category_id === id);
+    res.send(categoriesNews);
+  }
+});
 app.get("/news", (req, res) => {
   res.send(news);
+});
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((item) => item._id === id);
+  res.send(selectedNews);
 });
 
 app.listen(port, () => {
